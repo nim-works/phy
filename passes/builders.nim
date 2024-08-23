1772e8e5f08bd5e09ab829d26cbf7ffd7137fc57
zerbina <100542850+zerbina@users.noreply.github.com>
2024-08-23 23:39:52 +0000

    builders: extensions and fixes
    * fix `add` not working without enclosing tree
    * add an `initBuilder` overload that already starts a subtree
    * add an `add` overload for appending a full tree

--- passes/builders.nim
+++ passes/builders.nim
@@ -16,6 +16,10 @@ func initBuilder*[T](buf: sink seq[TreeNode[T]]): Builder[T] =
 func initBuilder*[T](): Builder[T] =
   Builder[T](parent: -1)
 
+func initBuilder*[T](nk: T): Builder[T] =
+  assert not isAtom(nk)
+  Builder[T](buf: @[TreeNode[T](kind: nk)], parent: 0)
+
 template subTree*[T](bu: var Builder[T], k: T, body: untyped) =
   ## Starts a new subtree of kind `k`.
   assert not isAtom(k)
@@ -32,8 +36,17 @@ template subTree*[T](bu: var Builder[T], k: T, body: untyped) =
 func add*[T](bu: var Builder[T], n: TreeNode[T]) =
   ## Appends atomic node `n` to the current subtree.
   assert isAtom(n.kind)
-  inc bu.buf[bu.parent].val
+  if bu.parent != -1:
+    inc bu.buf[bu.parent].val
   bu.buf.add n
+
+func add*[T](bu: var Builder[T], nodes: openArray[TreeNode[T]]) =
+  ## Appends all `nodes` to the current sub-tree. The nodes must either
+  ## represent a single atomic node, or a complete subtree.
+  assert nodes.len > 0
+  if bu.parent != -1:
+    inc bu.buf[bu.parent].val
+  bu.buf.add nodes
 
 func copyFrom*[T](bu: var Builder[T], tree: PackedTree[T], n: NodeIndex) =
   ## Inserts the whole subtree from `tree` at `n` at the current buffer
