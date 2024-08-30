47269855423ffc9b1e045c90c15e6864fd6a6d17
zerbina <100542850+zerbina@users.noreply.github.com>
2024-08-30 23:16:41 +0000

    tests: add the test runner for `pass10`
    It's a run-of-the-mill test runner. Given the repetition, common parts
    of the test runners should move into a separate module at one point.

---
+++ tests/pass10/runner.nim
@@ -0,0 +1,80 @@
+## The test runner for the pass10 tests.
+
+import
+  std/[
+    os,
+    streams,
+    strutils
+  ],
+  experimental/[
+    sexp
+  ],
+  passes/[
+    changesets,
+    debugutils,
+    pass0,
+    pass1,
+    pass3,
+    pass4,
+    pass10,
+    spec,
+    trees
+  ],
+  vm/[
+    vm,
+    vmenv,
+    vmvalidation,
+  ]
+
+let
+  args = getExecArgs()
+  s    = openFileStream(args[^1], fmRead)
+
+# skip the test specification:
+if s.readLine() == "discard \"\"\"":
+  while not s.readLine().endsWith("\"\"\""):
+    discard
+else:
+  s.setPosition(0)
+
+var tree = fromSexp[NodeKind](parseSexp("(Module " & readAll(s) & ")"))
+s.close()
+
+tree = tree.apply(pass10.lower(tree))
+
+# output the lowered code:
+stdout.writeLine(pretty(tree, tree.child(0)))
+stdout.writeLine(pretty(tree, tree.child(1)))
+stdout.writeLine(pretty(tree, tree.child(2)))
+stdout.write("!BREAK!")
+
+# apply the remaining lowerings:
+tree = tree.apply(pass4.lower(tree))
+tree = tree.apply(pass3.lower(tree, 8))
+tree = tree.apply(pass1.lower(tree, 8))
+
+# translate to VM bytecode:
+var env = initVm(1024, 1024 * 1024)
+translate(tree, env)
+
+# make sure the environment is correct:
+let errors = validate(env)
+if errors.len > 0:
+  for it in errors.items:
+    echo it
+  echo "validation failure"
+  quit(1)
+
+var
+  thread = vm.initThread(env, env.procs.high.ProcIndex, 1024, @[])
+  res = run(env, thread, nil)
+env.dispose(move thread)
+
+case res.kind
+of yrkDone:
+  stdout.write("(Done)")
+of yrkUnhandledException:
+  stdout.write("(UnhandledException " & $res.exc.intVal & ")")
+else:
+  echo "unexpected result, but got: ", res
+  quit(1)
