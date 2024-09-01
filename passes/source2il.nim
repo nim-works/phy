b28d542c121129f44e3179eb2d8f4bcb727d1816
zerbina <100542850+zerbina@users.noreply.github.com>
2024-09-01 14:13:29 +0000

    source2il: prepare for more built-in procedures
    Move the call analysis / code generation to a separate procedure.

--- passes/source2il.nim
+++ passes/source2il.nim
@@ -30,6 +30,7 @@ type
 
 using
   c: var Context
+  t: InTree
   bu: var Builder[NodeKind]
 
 template addType(c; kind: NodeKind, body: untyped): uint32 =
@@ -44,6 +45,51 @@ proc exprToIL(c; t: InTree, n: NodeIndex): (NodeSeq, TypeKind) =
   result[1] = exprToIL(c, t, n, bu)
   result[0] = finish(bu)
 
+proc binaryArithToIL(c; t; n: NodeIndex, name: string, bu): TypeKind =
+  ## Analyzes and emits the IL for a binary arithmetic call.
+  if t.len(n) != 3:
+    bu.add Node(kind: IntVal, val: 0) # add at least something
+    return tkError
+
+  let
+    (_, a, b)  = t.triplet(n)
+    (eA, typA) = exprToIL(c, t, a)
+    (eB, typB) = exprToIL(c, t, b)
+
+  let op =
+    case name
+    of "+": Add
+    of "-": Sub
+    else:   unreachable()
+
+  if typA == typB and typA != tkError:
+    let typ =
+      if typA == tkInt:
+        c.addType Int:
+          c.types.add Node(kind: Immediate, val: 8)
+      else:
+        c.addType Float:
+          c.types.add Node(kind: Immediate, val: 8)
+
+    bu.subTree op:
+      bu.add Node(kind: Type, val: typ)
+      bu.add eA
+      bu.add eB
+
+    result = typA
+  else:
+    bu.add Node(kind: IntVal)
+    result = tkError
+
+proc callToIL(c; t; n: NodeIndex, bu): TypeKind =
+  let name = t.getString(t.child(n, 0))
+  case name
+  of "+", "-":
+    result = binaryArithToIL(c, t, n, name, bu)
+  else:
+    bu.add Node(kind: IntVal)
+    result = tkError
+
 proc exprToIL(c; t: InTree, n: NodeIndex, bu): TypeKind =
   case t[n].kind
   of SourceKind.IntVal:
@@ -53,43 +99,7 @@ proc exprToIL(c; t: InTree, n: NodeIndex, bu): TypeKind =
     bu.add Node(kind: FloatVal, val: t[n].val)
     result = tkFloat
   of SourceKind.Call:
-    if t.len(n) == 3:
-      # only binary operations are supported at the moment
-      let (name, a, b) = triplet(t, n)
-      var op: NodeKind
-      case t.getString(name)
-      of "+": op = Add
-      of "-": op = Sub
-      else:
-        # TODO: proper error reporting is missing
-        bu.add Node(kind: IntVal, val: 0) # add at least something
-        return tkError
-
-      let
-        (eA, typA) = exprToIL(c, t, a)
-        (eB, typB) = exprToIL(c, t, b)
-
-      if typA == typB and typA != tkError:
-        let typ =
-          if typA == tkInt:
-            c.addType Int:
-              c.types.add Node(kind: Immediate, val: 8)
-          else:
-            c.addType Float:
-              c.types.add Node(kind: Immediate, val: 8)
-
-        bu.subTree op:
-          bu.add Node(kind: Type, val: typ)
-          bu.add eA
-          bu.add eB
-
-        result = typA
-      else:
-        bu.add Node(kind: IntVal, val: 0) # add at least something
-        result = tkError
-    else:
-      bu.add Node(kind: IntVal, val: 0) # add at least something
-      result = tkError
+    result = callToIL(c, t, n, bu)
   of AllNodes - ExprNodes:
     unreachable()
 
