a08f670e0fd0b93411c6ceac4f0c64d0d42c9155
zerbina <100542850+zerbina@users.noreply.github.com>
2024-09-02 23:15:22 +0200

    lang: implement `==`, `<=`, `<`, and `not` operators (#25)
    * implement the basic relational operators for integers, floats, and
      booleans
    * implement the logical `not` operator
    * implement the literal `true` and `false` values

--- passes/source2il.nim
+++ passes/source2il.nim
@@ -20,6 +20,7 @@ type
 
   TypeKind* = enum
     tkError
+    tkBool
     tkInt
     tkFloat
 
@@ -30,6 +31,7 @@ type
 
 using
   c: var Context
+  t: InTree
   bu: var Builder[NodeKind]
 
 template addType(c; kind: NodeKind, body: untyped): uint32 =
@@ -37,12 +39,109 @@ template addType(c; kind: NodeKind, body: untyped): uint32 =
     body
   (let r = c.numTypes; inc c.numTypes; r.uint32)
 
+proc typeToIL(c; typ: TypeKind): uint32 =
+  case typ
+  of tkBool:
+    c.addType Int: c.types.add(Node(kind: Immediate, val: 1))
+  of tkInt, tkError:
+    c.addType Int: c.types.add(Node(kind: Immediate, val: 8))
+  of tkFloat:
+    c.addType Float: c.types.add(Node(kind: Immediate, val: 8))
+
 proc exprToIL(c; t: InTree, n: NodeIndex, bu): TypeKind
 
 proc exprToIL(c; t: InTree, n: NodeIndex): (NodeSeq, TypeKind) =
   var bu = initBuilder[NodeKind]()
   result[1] = exprToIL(c, t, n, bu)
   result[0] = finish(bu)
+
+template lenCheck(t; n: NodeIndex, bu; expected: int) =
+  ## Exits the current analysis procedure with an error, if `n` doesn't have
+  ## `expected` children.
+  if t.len(n) != expected:
+    bu.add Node(kind: IntVal)
+    return tkError
+
+proc binaryArithToIL(c; t; n: NodeIndex, name: string, bu): TypeKind =
+  ## Analyzes and emits the IL for a binary arithmetic operation.
+  lenCheck(t, n, bu, 3)
+
+  let
+    (_, a, b)  = t.triplet(n)
+    (eA, typA) = exprToIL(c, t, a)
+    (eB, typB) = exprToIL(c, t, b)
+
+  let op =
+    case name
+    of "+": Add
+    of "-": Sub
+    else:   unreachable()
+
+  if typA == typB and typA in {tkInt, tkFloat}:
+    let typ = c.typeToIL(typA)
+
+    bu.subTree op:
+      bu.add Node(kind: Type, val: typ)
+      bu.add eA
+      bu.add eB
+
+    result = typA
+  else:
+    bu.add Node(kind: IntVal)
+    result = tkError
+
+proc relToIL(c; t; n: NodeIndex, name: string, bu): TypeKind =
+  ## Analyzes and emits the IL for a relational operation.
+  lenCheck(t, n, bu, 3)
+
+  let
+    (_, a, b)  = t.triplet(n)
+    (eA, typA) = exprToIL(c, t, a)
+    (eB, typB) = exprToIL(c, t, b)
+
+  let (op, valid) =
+    case name
+    of "==": (Eq, {tkBool, tkInt, tkFloat})
+    of "<":  (Lt, {tkInt, tkFloat})
+    of "<=": (Le, {tkInt, tkFloat})
+    else:   unreachable()
+
+  if typA == typB and typA in valid:
+    bu.subTree op:
+      bu.add Node(kind: Type, val: c.typeToIL(typA))
+      bu.add eA
+      bu.add eB
+
+    result = tkBool
+  else:
+    bu.add Node(kind: IntVal)
+    result = tkError
+
+proc notToIL(c; t; n: NodeIndex, bu): TypeKind =
+  lenCheck(t, n, bu, 2)
+
+  let (arg, typ) = exprToIL(c, t, t.child(n, 1))
+
+  if typ == tkBool:
+    bu.subTree Not:
+      bu.add arg
+    result = tkBool
+  else:
+    bu.add Node(kind: IntVal)
+    result = tkError
+
+proc callToIL(c; t; n: NodeIndex, bu): TypeKind =
+  let name = t.getString(t.child(n, 0))
+  case name
+  of "+", "-":
+    result = binaryArithToIL(c, t, n, name, bu)
+  of "==", "<", "<=":
+    result = relToIL(c, t, n, name, bu)
+  of "not":
+    result = notToIL(c, t, n, bu)
+  else:
+    bu.add Node(kind: IntVal)
+    result = tkError
 
 proc exprToIL(c; t: InTree, n: NodeIndex, bu): TypeKind =
   case t[n].kind
@@ -52,44 +151,19 @@ proc exprToIL(c; t: InTree, n: NodeIndex, bu): TypeKind =
   of SourceKind.FloatVal:
     bu.add Node(kind: FloatVal, val: t[n].val)
     result = tkFloat
-  of SourceKind.Call:
-    if t.len(n) == 3:
-      # only binary operations are supported at the moment
-      let (name, a, b) = triplet(t, n)
-      var op: NodeKind
-      case t.getString(name)
-      of "+": op = Add
-      of "-": op = Sub
-      else:
-        # TODO: proper error reporting is missing
-        bu.add Node(kind: IntVal, val: 0) # add at least something
-        return tkError
-
-      let
-        (eA, typA) = exprToIL(c, t, a)
-        (eB, typB) = exprToIL(c, t, b)
-
-      if typA == typB and typA != tkError:
-        let typ =
-          if typA == tkInt:
-            c.addType Int:
-              c.types.add Node(kind: Immediate, val: 8)
-          else:
-            c.addType Float:
-              c.types.add Node(kind: Immediate, val: 8)
-
-        bu.subTree op:
-          bu.add Node(kind: Type, val: typ)
-          bu.add eA
-          bu.add eB
-
-        result = typA
-      else:
-        bu.add Node(kind: IntVal, val: 0) # add at least something
-        result = tkError
+  of SourceKind.Ident:
+    case t.getString(n)
+    of "false":
+      bu.add Node(kind: IntVal, val: 0)
+      result = tkBool
+    of "true":
+      bu.add Node(kind: IntVal, val: 1)
+      result = tkBool
     else:
-      bu.add Node(kind: IntVal, val: 0) # add at least something
+      bu.add Node(kind: IntVal)
       result = tkError
+  of SourceKind.Call:
+    result = callToIL(c, t, n, bu)
   of AllNodes - ExprNodes:
     unreachable()
 
@@ -100,12 +174,7 @@ proc exprToIL*(t: InTree): (TypeKind, PackedTree[NodeKind]) =
 
   let
     (e, typ) = exprToIL(c, t, NodeIndex(0))
-    typId =
-      case typ
-      of tkInt, tkError:
-        c.addType Int: c.types.add(Node(kind: Immediate, val: 8))
-      of tkFloat:
-        c.addType Float: c.types.add(Node(kind: Immediate, val: 8))
+    typId = c.typeToIL(typ)
 
     ptypId = c.addType ProcTy:
       c.types.add Node(kind: Type, val: typId)
