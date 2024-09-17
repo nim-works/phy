d0667e5049b31e28831bb5e5c117b3ad6b7068a1
zerbina <100542850+zerbina@users.noreply.github.com>
2024-09-17 21:31:44 +0000

    reporting: use specialized reporting procedures
    Instead of one single `report` procedure, there is now a separate
    procedure for errors, warnings, hints, and debug messages.

--- phy/default_reporting.nim
+++ phy/default_reporting.nim
@@ -3,7 +3,7 @@ import
 
 type
   DefaultReporter*[T] = object of ReportContext[T]
-    ## A report context that only accumulates messages, but doesn't do
+    ## A report context that only accumulates diagnostics, but doesn't do
     ## anything with them.
     reports: seq[T]
 
@@ -15,11 +15,11 @@ type
 proc initDefaultReporter*[T](): ref DefaultReporter[T] =
   ## Sets up a default reporter.
   new(result)
-  result.sendPrc = proc(self: ref ReportContext[T], rep: sink T) =
+  result.errorPrc = proc(self: ref ReportContext[T], rep: sink T) =
     (ref DefaultReporter[T])(self).reports.add rep
   result.fatalPrc = proc(self: ref ReportContext[T], rep: sink T) =
     raise (ref FatalError[T])(rep: rep)
 
 proc retrieve*[T](r: var DefaultReporter[T]): seq[T] =
-  ## Takes all accumulated reports from `r`.
+  ## Takes all accumulated error diagnostics from `r`.
   result = move r.reports
--- phy/reporting.nim
+++ phy/reporting.nim
@@ -8,18 +8,44 @@ import
 type
   ReportContext*[T] = object of RootObj
     ## The base type that all report context types must be derived from.
-    sendPrc*: proc(c: ref ReportContext[T], rep: sink T)
-      ## called when a message is sent to the context
-    fatalPrc*: proc(c: ref ReportContext[T], rep: sink T)
-      ## called when a fatal message is sent to the context. The procedure
+    debugPrc*: proc(c: ref ReportContext[T], diag: sink T)
+      ## may be nil
+    hintPrc*: proc(c: ref ReportContext[T], diag: sink T)
+      ## may be nil
+    warnPrc*: proc(c: ref ReportContext[T], diag: sink T)
+      ## may be nil
+    errorPrc*: proc(c: ref ReportContext[T], diag: sink T)
+      ## called when an error diagnostic is sent to the context. The procedure
+      ## is not required to return normally
+    fatalPrc*: proc(c: ref ReportContext[T], diag: sink T)
+      ## called when a fatal diagnostic is sent to the context. The procedure
       ## must not return normally
 
-proc report*[T](c: ref ReportContext[T], rep: sink T) =
-  ## Sends `rep` to the report context.
-  c.sendPrc(c, rep)
+proc debug*[T](c: ref ReportContext[T], diag: sink T) =
+  ## Sends the debug diagnostic `diag` to the report context.
+  if c.debugPrc != nil:
+    c.debugPrc(c, diag)
 
-proc fatal*[T](c: ref ReportContext[T], rep: sink T) {.noreturn.} =
-  ## Sends `rep` to the report context, which is guaranteed to result in the
-  ## current control-flow path to terminate.
-  c.fatalPrc(rep)
+proc hint*[T](c: ref ReportContext[T], diag: sink T) =
+  ## Sends the hint diagnostic `diag` to the report context. This is meant
+  ## for user-facing hints (about some input, like code).
+  if c.hintPrc != nil:
+    c.hintPrc(c, diag)
+
+proc warn*[T](c: ref ReportContext[T], diag: sink T) =
+  ## Sends the error diagnostic `diag` to the report context. This is meant
+  ## for user-facing warnings (about some input, like code).
+  if c.warnPrc != nil:
+    c.warnPrc(c, diag)
+
+proc error*[T](c: ref ReportContext[T], diag: sink T) =
+  ## Sends the error diagnostic `diag` to the report context. This is meant
+  ## for user-facing errors. The callsite has to assume that ``error`` returns
+  ## normally.
+  c.errorPrc(c, diag)
+
+proc fatal*[T](c: ref ReportContext[T], diag: sink T) {.noreturn.} =
+  ## Sends the error diagnostic `diag` to the report context, which is
+  ## guaranteed to result in the current control-flow path to terminate.
+  c.fatalPrc(diag)
   unreachable()
