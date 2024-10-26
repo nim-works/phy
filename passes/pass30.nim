4ceed496fb145ac189f266a9d593b831970575ec
zerbina <100542850+zerbina@users.noreply.github.com>
2024-10-26 16:24:58 +0000

    pass30: remove `Placeholder` usage
    Instead of using placeholder to patch jump labels at a later point, the
    exit label for a block is now allocated when first needed, removing the
    need to keep a `seq` per context (which is quite heavy on the
    allocator).

--- passes/pass30.nim
+++ passes/pass30.nim
@@ -3,11 +3,13 @@
 ## control-flow statements.
 
 import
+  std/options,
   passes/[builders, changesets, trees, spec],
   vm/utils
 
 type
   Node = TreeNode[NodeKind]
+  LabelId = uint32
 
   PassCtx = object
     types: NodeIndex
@@ -18,7 +20,9 @@ type
     nextLabel: uint32
       ## the ID to use for the next label. Incremented when "allocating" a
       ## label
-    blockExits: seq[seq[Placeholder]]
+    context: seq[Option[LabelId]]
+      ## stack of block context objects. For each block-like strucutre, a new
+      ## entry is pushed
 
 # shorten some common procedure signatures:
 using
@@ -32,48 +36,49 @@ func imm(n: Node): uint32 {.inline.} =
   assert n.kind == Immediate
   n.val
 
-proc pushContext(c) =
-  c.blockExits.add default(seq[Placeholder])
-
-proc join(c; bu): uint32 =
-  result = c.nextLabel
+proc newLabel(c): LabelId =
+  result = c.nextLabel.LabelId
   inc c.nextLabel
 
-  bu.subTree Join:
-    bu.add Node(kind: Immediate, val: result)
+proc reserve(c; label: var Option[LabelId]): LabelId =
+  ## Unwraps `label`, allocating a new label and updating `label` with it if
+  ## `label` is not yet initialized.
+  if label.isSome:
+    result = label.unsafeGet
+  else:
+    result = c.newLabel()
+    label = some(result)
 
-proc complete(c; p: sink Placeholder, label: uint32, bu) =
-  bu.replace(p, Node(kind: Immediate, val: label))
+proc join(bu; label: LabelId) =
+  bu.subTree Join:
+    bu.add Node(kind: Immediate, val: label)
+
+proc goto(bu; label: LabelId) =
+  bu.subTree Continue:
+    bu.add Node(kind: Immediate, val: label)
+
+proc pushContext(c) =
+  c.context.add none(LabelId)
 
 proc popContext(c; bu): bool =
   ## Pops the context for some structured control-flow construct. Returns
   ## 'true' when structured control-flow never leaves the construct, false
   ## otherwise.
-  var exits = c.blockExits.pop()
-  result = exits.len == 0
+  let label = c.context.pop()
+  result = label.isNone # no label means "exit is never reached"
   if not result:
-    let label = c.join(bu)
-    for e in exits.mitems:
-      c.complete(move e, label, bu)
+    bu.join(label.unsafeGet)
 
-proc leave(c; p: sink Placeholder, blk: int) =
-  ## Registers a jump to the end of the given context. `p` is later replaced
-  ## with the label of the join point.
-  c.blockExits[^blk].add p
+proc leave(c; blk: int): LabelId =
+  ## Registers a jump to the exit of the given block-like control-flow
+  ## construct.
+  c.reserve(c.context[^blk])
 
 proc lowerStmt(c; tree; n, bu): bool
 
 proc lowerBody(c; tree; n, bu) =
   if not c.lowerStmt(tree, n, bu):
-    bu.subTree Continue:
-      c.leave(bu.placeholder(), 1)
-
-{.push warning[ProveInit]: off.}
-proc hack*(): Placeholder =
-  ## Used to initialize a ``Placeholder`` variable where the compiler isn't
-  ## able to figure that a local is in fact assigned to before it's used.
-  discard
-{.pop.}
+    bu.goto(c.leave(1))
 
 proc lowerStmt(c; tree; n, bu): bool =
   ## Lowers an |L10| nested statement into a flat sequence of statements,
@@ -86,8 +91,7 @@ proc lowerStmt(c; tree; n, bu): bool =
       discard
     result = true
   of Break:
-    bu.subTree Continue:
-      c.leave(bu.placeholder(), tree[n, 0].imm.int + 1)
+    bu.goto(c.leave(tree[n, 0].imm.int + 1))
     result = true
   of Return:
     bu.copyFrom(tree, n)
@@ -108,7 +112,8 @@ proc lowerStmt(c; tree; n, bu): bool =
     c.lowerBody(tree, tree.child(n, 0), bu)
     result = c.popContext(bu)
   of Loop:
-    let label = c.join(bu)
+    let label = c.newLabel()
+    bu.join(label)
 
     c.pushContext()
     if not c.lowerStmt(tree, tree.child(n, 0), bu):
@@ -118,37 +123,41 @@ proc lowerStmt(c; tree; n, bu): bool =
 
     result = c.popContext(bu)
   of If:
-    # XXX: language shortcoming workaround. Both then and els are
-    #      initialized before their first use, but not explicitly
-    #      initializing them as part of the declaration is an error...
-    var then, els = hack()
-
-    bu.subTree SelectBool:
-      bu.copyFrom(tree, tree.child(n, 0))
-      bu.subTree Continue: # the false branch
-        els = bu.placeholder()
-      bu.subTree Continue: # the true branch
-        then = bu.placeholder()
-
     c.pushContext()
     case tree.len(n)
     of 2: # if-then
-      let (_, a) = tree.pair(n)
-      c.complete(then, c.join(bu), bu)
+      let
+        (cond, a) = tree.pair(n)
+        then      = c.newLabel()
+
+      bu.subTree SelectBool:
+        bu.copyFrom(tree, cond)
+        bu.goto(c.leave(1)) # the false branch
+        bu.goto(then)       # the true branch
+
+      bu.join(then)
       c.lowerBody(tree, a, bu)
-      c.leave(els, 1)
     of 3: # if-then-else
-      let (_, a, b) = tree.triplet(n)
-      c.complete(then, c.join(bu), bu)
+      let
+        (cond, a, b) = tree.triplet(n)
+        then         = c.newLabel()
+        els          = c.newLabel()
+
+      bu.subTree SelectBool:
+        bu.copyFrom(tree, cond)
+        bu.goto(els)  # the false branch
+        bu.goto(then) # the true branch
+
+      bu.join(then)
       c.lowerBody(tree, a, bu)
-      c.complete(els, c.join(bu), bu)
+      bu.join(els)
       c.lowerBody(tree, b, bu)
     else:
       unreachable()
 
     result = c.popContext(bu)
   of Case:
-    var branches = newSeq[(NodeIndex, Placeholder)]()
+    var branches = newSeq[(NodeIndex, LabelId)]()
     # emit the Select statement and gather the case branches:
     bu.subTree Select:
       bu.copyFrom(tree, tree.child(n, 0))
@@ -156,13 +165,14 @@ proc lowerStmt(c; tree; n, bu): bool =
       for it in tree.items(n, 2):
         bu.subTree Choice:
           bu.copyFrom(tree, tree.child(it, 0))
-          bu.subTree Continue:
-            branches.add (tree.child(it, 1), bu.placeholder())
+          let label = c.newLabel()
+          branches.add (tree.child(it, 1), label)
+          bu.goto(label)
 
     # emit the case branch bodies:
     c.pushContext()
-    for (pos, ph) in branches.mitems:
-      c.complete(move ph, c.join(bu), bu)
+    for (pos, label) in branches.mitems:
+      bu.join(label)
       c.lowerBody(tree, pos, bu)
 
     result = c.popContext(bu)
@@ -176,7 +186,6 @@ proc lowerStmt(c; tree; n, bu): bool =
     bu.copyFrom(tree, n)
 
 proc lowerProc(c: var PassCtx, tree; n; changes) =
-  c.blockExits.shrink(0)
   c.nextLabel = 0
   c.locals = tree.child(n, 1)
 
@@ -184,6 +193,8 @@ proc lowerProc(c: var PassCtx, tree; n; changes) =
     # the body of a procedure must always end with a terminator
     doAssert c.lowerStmt(tree, tree.child(n, 2), bu),
               "control-flow falls out of the body"
+
+  assert c.context.len == 0, "context stack is not empty"
 
 proc lower*(tree): Changeset[NodeKind] =
   ## Computes the changeset representing the lowering for a whole module
