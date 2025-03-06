226501f1490846754109cd328e47049d6ee31d9d
zerbina <100542850+zerbina@users.noreply.github.com>
2025-03-06 22:08:24 +0000

    source2il: implement `As`
    The implementation is straightforward: making the sure the target type
    is valid, fitting the source expression to the target type, and
    discarding the type qualifiers is enough.
    
    Error correction (the lack thereof) for fitting errors is also fixed,
    so that no ill-formed `Expr`s are created.

--- passes/syntax_source.nim
+++ passes/syntax_source.nim
@@ -20,6 +20,7 @@ type
     TupleCons
     Seq
     FieldAccess, At
+    As
     Exprs
     Asgn
     Return
@@ -34,7 +35,8 @@ type
 
 const
   ExprNodes* = {IntVal, FloatVal, Ident, And, Or, If, While, Call, TupleCons,
-                Seq, FieldAccess, At, Asgn, Return, Unreachable, Exprs, Decl}
+                Seq, FieldAccess, At, As, Asgn, Return, Unreachable, Exprs,
+                Decl}
   DeclNodes* = {ProcDecl, TypeDecl}
   AllNodes* = {low(NodeKind) .. high(NodeKind)}
 
