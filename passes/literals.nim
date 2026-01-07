b05581fe355b5648e2354fad6a4e545f35e7a1f6
zerbina <100542850+zerbina@users.noreply.github.com>
2026-01-07 00:08:53 +0000

    literals: implement small-value optimization

--- passes/literals.nim
+++ passes/literals.nim
@@ -1,7 +1,6 @@
 ## Implements the storage for literal data embedded in ASTs.
 
 # TODO: use bi-tables for the number and string values
-# TODO: store small integers inline (like how the packing in `trees` does it)
 
 type
   Literals* = object
@@ -9,11 +8,33 @@ type
     numbers*: seq[uint64] ## a list of bit patterns
     strings*: seq[string]
 
+const
+  OverflowShift = 31
+  OverflowBit   = 1'u32 shl OverflowShift
+    ## use the most significant bit to flag whether a value is larger than
+    ## `max(int32)` and overflows into `PackedTree.numbers`
+
+template castAny[T, U](x: U): T =
+  ## A `cast` that doesn't warn when casting into the origin type.
+  when T is U: x
+  else:        cast[T](x)
+
 proc pack*(s: var Literals, val: SomeInteger): uint32 {.inline.} =
   ## Adds the bit-representation of `val` to `s` and returns an ID to refer
   ## to the value with later.
-  result = uint32(s.numbers.len)
-  s.numbers.add cast[uint64](val)
+  when sizeof(val) < 4:
+    # always fits into the packed representation
+    result = cast[uint32](val)
+  else:
+    if (val shr OverflowShift) != 0:
+      # overflows the packed value range
+      result = uint32(s.numbers.len) or OverflowBit
+      when val is SomeSignedInt:
+        s.numbers.add castAny[uint64](int64(val)) # sign extend
+      else:
+        s.numbers.add castAny[uint64](val)
+    else:
+      result = castAny[uint32](val) # fits the packed range
 
 proc pack*(s: var Literals, val: float): uint32 {.inline.} =
   ## Adds the bit-representation of `val` to `s` and returns an ID to refer
@@ -28,10 +49,10 @@ proc pack*(s: var Literals, val: string): uint32 {.inline.} =
 
 proc unpack*[T: SomeNumber](s: Literals, id: uint32, _: typedesc[T]): T {.inline.} =
   ## Returns the bit-representation stored under `id` interpreted as `T`.
-  when T is uint64: # prevent warnings
-    s.numbers[id]
+  if (id and OverflowBit) != 0:
+    castAny[T](s.numbers[id and not OverflowBit])
   else:
-    cast[T](s.numbers[id])
+    castAny[T](id)
 
 proc unpack*(s: Literals, id: uint32, _: typedesc[string]): lent string {.inline.} =
   ## Returns the string stored under `id`.
