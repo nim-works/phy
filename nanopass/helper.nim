70fa15490ad75ec9b95c1a6943cec3e629688280
zerbina <100542850+zerbina@users.noreply.github.com>
2026-01-20 22:10:02 +0000

    helper: remove `copyLineInfoForTree`

--- nanopass/helper.nim
+++ nanopass/helper.nim
@@ -2,13 +2,8 @@
 
 import std/[macros]
 
-proc copyLineInfoForTree*(n, info: NimNode) =
-  copyLineInfo(n, info)
-  for i in 0..<n.len:
-    copyLineInfoForTree(n[i], info)
-
 proc makeError*(msg: string, info: NimNode): NimNode =
   ## Creates an error statement reporting `msg` at the given source location.
   let it = nnkExprColonExpr.newTree(ident"error", newStrLitNode(msg))
+  copyLineInfo(it, info)
   result = nnkPragma.newTree(it)
-  copyLineInfoForTree(result, info)
